fe13af90dea14e9c0aa264051796ba1d8b1acae9
André L O Júnior <145133907+andreojr@users.noreply.github.com>
2024-08-01 16:14:44 -0300

    Update testbench.v

--- verilog/testbench.v
+++ verilog/testbench.v
@@ -1,59 +1,51 @@
 `include "./verilog/mealy.v"
+`include "./verilog/moore.v"
 
 module testbench;
 	reg clk, head, left;
   	wire front, rotate;
   
-  	mealy_robot MR(clk, head, left, front, rotate);
+  	moore_robot MR(clk, head, left, front, rotate);
   	
-  	initial begin
-        clk = 0;
-        step(0,0);
-        step(0,0);
-        step(1,0);
-        step(0,0);
-        step(0,0);
-        step(0,1);
-        step(0,1);
-        step(0,1);
-        step(0,1);
-        step(1,1);
-        step(1,0);
-        step(0,0);
-        step(0,1);
-        step(1,0);
-        step(0,0);
-        step(1,1);
-        step(1,0);
-        step(0,0);
-        step(0,1);
-        step(0,1);
-        step(0,1);
-    end
+  	initial
+    	begin
+          clk = 0;
+          step(0,0);
+          step(0,0);
+          step(1,0);
+          step(0,0);
+          step(0,0);
+          step(0,1);
+          step(0,1);
+          step(0,1);
+          step(0,1);
+          step(1,1);
+          step(1,0);
+          step(0,0);
+          step(0,1);
+          step(1,0);
+          step(0,0);
+          step(1,1);
+          step(1,0);
+          step(0,0);
+          step(0,1);
+          step(0,1);
+          step(0,1);
+        end
   	
   	task toggle_clock;
-        begin
-            #1 clk = ~clk;
-            #2 clk = ~clk;
-        end
+      	#1 clk = ~clk;
+    	#2 clk = ~clk;
     endtask
  	
-    task step (input h, l);
-        begin
-            head = h;
-            left = l;
-            toggle_clock;
-        end  	
+  task step(input h, l);
+    	head = h;
+    	left = l;
+    	toggle_clock;
+    	display;
     endtask
   	  
-    task display (string cs, fs);
-        begin
-            string out;
-            case ({front, rotate})
-                2'b10: out = "frente";
-                2'b01: out = "rotaciona";
-            endcase
-            $display("%s | head: %b left: %b | %s | %s", cs, head, left, out, fs);
-        end
-    endtask
+  task display;
+    $display("head:%b,left:%b,front:%b,rotate: %b", head, left, front, rotate);
+  endtask
 endmodule
