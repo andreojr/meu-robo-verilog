f522539f2a8b621b2de3ddfe90344051c1f141a2
e-fontana <contato@eduardofontana.dev>
2024-08-01 16:58:41 -0300

    refactor: file to csv format

--- accuracy.py
+++ accuracy.py
@@ -1,6 +1,8 @@
+import csv
+
 class Test:
     def __init__(self):
-        self.output = open("output.txt", "r").readlines()
+        self.output_path = "./build/output.csv"
         self.columns = "ABCDEFGHIJKLMNO"
         self.lines = "01234567"
         self.right_way = ["C3↑", "C2↑", "C1↑", "C1←", "C1↓", "C1→", "D1→", "E1→", "F1→", "G1→", "G1↑", "G1←", "G1↓", "G2↓", "G2→", "H2→", "H2↑", "H2←", "H2↓", "H3↓", "H4↓"]
@@ -24,7 +26,7 @@ class Test:
             case "→":
                 next_pos = [self.cur_pos[0]+1, self.cur_pos[1]]
         self.cur_pos = next_pos
-    
+
     def rotate(self):
         match self.looking_for:
             case "↑":
@@ -38,17 +40,18 @@ class Test:
 
     def generate_data(self):
         self.data = []
-        for line in self.output:
-            data_line = {}
-            line = [arg.strip() for arg in line.split(",")]
-            data_line["head"] = int(line[0][-1])
-            data_line["left"] = int(line[1][-1])
-            data_line["out"] = "frente" if (line[2][-1] + line[3][-1] == "10") else "rotaciona"
-            self.data.append(data_line)
-    
+        with open(self.output_path, "r", encoding='utf-8') as file:
+            reader = csv.reader(file, delimiter=",")
+            for row in reader:
+                data_line = {}
+                data_line["head"] = int(row[0][-1])
+                data_line["left"] = int(row[1][-1])
+                data_line["out"] = "frente" if (row[2][-1] + row[3][-1] == "10") else "rotaciona"
+                self.data.append(data_line)
+
     def display_position(self):
         return self.columns[self.cur_pos[0]] + self.lines[self.cur_pos[1]] + self.looking_for
-    
+
     def run(self):
         for pos, step in enumerate(self.data):
             if self.display_position() == self.right_way[pos]:
@@ -59,7 +62,4 @@ class Test:
                 self.forward()
             elif step["out"] == "rotaciona":
                 self.rotate()
-        print()
-
-
 Test().run()
